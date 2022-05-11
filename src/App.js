7ae6935d361f68c7f330eac8888905f6ac07519a
Tomer Shmul <shmul.tomer@gmail.com>
2022-05-11 16:47:42 -0400

    Zoom changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import styled from 'styled-components'
 import { useEffect, useState } from 'react';
 import { useCallback } from 'react';
+import {FaLinkedin, FaEnvelope, FaInstagramSquare} from 'react-icons/fa';
+import {ImMail} from 'react-icons/im'
+
 
 function App() {
 
@@ -27,6 +30,7 @@ function App() {
     <>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
     <meta name="theme-color" content="#5986fb"></meta> 
+    <meta name="msapplication-TileColor" content="#da532c"></meta>
     <div className="App">
       <header className="App-header">
           Tomer Shmul
@@ -34,6 +38,18 @@ function App() {
       <div className="App-header2">
         {newName}
       </div>
+      <div className="icons">
+        <a className="icon" href="google.com">
+            <FaLinkedin />
+        </a>
+        <a className="icon" href="google.com">
+            <ImMail />
+        </a>
+        <a className="icon" href="google.com">
+            <FaInstagramSquare />
+        </a>
+      </div>
+      
     </div>
     </>
   );
