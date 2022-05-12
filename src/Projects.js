dd0a0566ef3373a7ef13ce2d893fd887d1dabc87
Tomer Shmul <shmul.tomer@gmail.com>
2022-05-11 22:22:45 -0400

    Font changes

--- src/Projects.js
+++ src/Projects.js
@@ -6,6 +6,8 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
+
+
 const Projects = () => {
   return (
     <div className="proj">
@@ -14,6 +16,25 @@ const Projects = () => {
         </div>
         <div className="cards">
             <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea href="https://www.gtcomplaint.com/" target="_blank">
+                    <CardMedia
+                        component="img"
+                        height="100"
+                        image="https://i.ibb.co/yRJLq0M/GT-Complaint-MUP.jpg"
+                        alt="project image"
+                        />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        GT Complaint Website
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        I created a website that allows users to create accounts using their school email,
+                        submit complaints, respond to other complaints, and resolve when they are addressed.
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+            <Card sx={{ maxWidth: 345 }}>
                 <CardActionArea>
                     <CardMedia
                         component="img"
@@ -23,7 +44,26 @@ const Projects = () => {
                         />
                     <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                        GT Complaint Website
+                        Hive Defense Game
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        An Android game where you can place defending towers around a path where different enemies will 
+                        try to approach and destroy the hive at the end. Towers can be upgraded, which is needed to defeat the final boss.
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+                        alt="green iguana"
+                        />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        DriveSafe Prototype
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                         Lizards are a widespread group of squamate reptiles, with over 6,000
@@ -42,45 +82,7 @@ const Projects = () => {
                         />
                     <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                        GT Complaint Website
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
-                        alt="green iguana"
-                        />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        GT Complaint Website
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
-                        alt="green iguana"
-                        />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        GT Complaint Website
+                        XXX
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                         Lizards are a widespread group of squamate reptiles, with over 6,000
